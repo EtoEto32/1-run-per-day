2a6db6f2de05f8e82763d7c0992594495e64b8d5
Ettotto94 <Ace09040805@gmail.com>
2024-03-04 22:15:03 +0900

    3/4ファイル整理

---
+++ paiza/javascript入門編/1_5.js
@@ -0,0 +1,36 @@
+// 変数を使う
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var Player="勇者";
+var number=3;//variable変数の意
+console.log(Player)
+console.log(Player+"は、荒野を歩いていた");
+console.log(Player+"は、モンスターと戦った");
+console.log(Player+"は、モンスターをたおした");
+
+// 変数を使う
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var player = "勇者";
+console.log(player+"は、レベルアップした");
+
+// 変数を使う
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var player = "勇者";
+console.log(player+ "の体力が回復した");
+
+// 変数を使う
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var teams = "勇者と戦士";
+console.log(player+ "の体力が回復した");
+
+// 変数を使う
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var player = "勇者";
+
+console.log(player+"は、荒野を歩いていた");
+console.log(player+"は、モンスターと戦った");
+console.log(player+"は、モンスターをたおした");
