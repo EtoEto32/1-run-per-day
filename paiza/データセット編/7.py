df11c4a19fbd531545523daeb47d7fa8f1715abd
Ettotto94 <Ace09040805@gmail.com>
2024-03-19 10:53:59 +0900

    3/19

--- paiza/データセット編/7.py
+++ paiza/データセット編/7.py
@@ -1,5 +1,5 @@
 H, W, r, c = map(int, input().split())
-maze = [input() for _ in range(H)]#1次元配列に格納
+maze = [input() for _ in range(H)]#1次元配列に格納、実際には2次元配列
 
 if maze[r - 1][c - 1] == "#":#配列の中にある文字を指定している
     print("Yes")
