d00b988cbe9e95ac6fef50f1573837fa14b2d436
Ettotto94 <Ace09040805@gmail.com>
2024-03-19 10:45:46 +0900

    3/19

---
+++ paiza/データセット編/7.py
@@ -0,0 +1,17 @@
+H, W, r, c = map(int, input().split())
+maze = [input() for _ in range(H)]#1次元配列に格納
+
+if maze[r - 1][c - 1] == "#":#配列の中にある文字を指定している
+    print("Yes")
+else:
+    print("No")
+#入力例と出力例
+#
+# 3 4 1 2
+#   ..#.
+#   #.##
+#   ....
+
+
+#出力例1
+#No
