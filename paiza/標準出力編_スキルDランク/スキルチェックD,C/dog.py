5a5fab23475cc7253faa10bbe860a7dcac298bd1
Ettotto94 <Ace09040805@gmail.com>
2024-05-12 02:36:40 +0900

    5/12

--- paiza/標準出力編_スキルDランク/スキルチェックD,C/dog.py
+++ paiza/標準出力編_スキルDランク/スキルチェックD,C/dog.py
@@ -6,7 +6,7 @@ f1, f2 = map(int, input().split())
 
 # 1回戦の結果を格納する辞書を作成
 first_round = {1: e1, 2: e2, 3: e3, 4: e4}
-
+#絶対的に指定する事でエントリー番号を正しく指定する事ができる
 # 1回戦の勝者を決定
 winners = []
 if first_round[p1] < first_round[p2]:
@@ -27,7 +27,7 @@ second_round[winners[1]] = f2
 
 # 優勝者と準優勝者を決定
 winner = min(second_round, key=second_round.get)
-del second_round[winner]
+del second_round[winner]#優勝者の分を削除
 runner_up = min(second_round, key=second_round.get)
 
 # 結果を出力
