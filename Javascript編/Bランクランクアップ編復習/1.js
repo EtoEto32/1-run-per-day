b3e20c423f1b97685d4de42bcfaeca44b27a7397
EtoEto32 <c1204041@st.kanazawa-it.ac.jp>
2024-12-02 13:02:33 +0900

    jsきたあああああああああああああああああああああ

---
+++ Javascript編/Bランクランクアップ編復習/1.js
@@ -0,0 +1,16 @@
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+// 自分の得意な言語で
+// Let's チャレンジ！！
+var lines = [];
+var reader = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+reader.on("line", (line) => {
+  lines.push(line);
+});
+reader.on("close", () => {
+  lines[0]++;
+  console.log(lines[0]);
+});
