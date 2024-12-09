7b426442282e68c817b9faaf804e424bea1bac1b
EtoEto32 <c1204041@st.kanazawa-it.ac.jp>
2024-12-09 19:31:54 +0900

    javascriptの標準入力ゴミすぎワロタwwwwww

---
+++ Javascript編/配列編/1.js
@@ -0,0 +1,6 @@
+const li = [5, 1, 3, 4, 5, 12, 6, 8, 1, 3];
+var count = 0;
+for (x of li) {
+  count++;
+}
+console.log(count);
---
+++ Javascript編/配列編/2.js
@@ -0,0 +1,5 @@
+const li = [5, 1, 3, 4, 5, 12, 6, 8, 1, 3];
+for (x of li) {
+  console.log(x);
+}
+
