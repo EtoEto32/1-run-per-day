006594d2359477fe5d8f7782da0a9e3a772cde69
EtoEto32 <c1204041@st.kanazawa-it.ac.jp>
2024-12-10 23:46:34 +0900

    おおかたつくれた

---
+++ python/未分類/hit_and_blow.py
@@ -0,0 +1,36 @@
+"""
+4桁のヒット＆ブローを開発する。
+自分：自分で入力
+相手：ランダムに入力。所謂:CPU
+
+Regulation：
+https://45mix.net/hit-and-blow-game/
+"""
+
+#blowを判定する関数
+#hitを判定する関数
+#勝敗判定する関数
+#cpuのターン関数
+#自分のターン関数
+
+import random
+
+player_data = []
+cpu_data = []
+my_guess=[]
+cpu_guess=[]
+
+#初期化
+
+
+def my_turn():
+
+
+def your_turn():
+  
+
+
+def Function_to_determine_blow():
+
+def Function_to_determine_hit():
+  
